91bbb3ee5b2dd6b3751c8a560312e78c6efdf449
Gaks3 <dwi66116@gmail.com>
2024-03-31 15:19:17 +0700

    feat: return back url not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,18 +1,22 @@
-import Link from 'next/link'
+'use client'
+
+import { useRouter } from 'next/navigation'
 
 const Page = () => {
+  const router = useRouter()
+
   return (
-    <div className="min-h-screen max-w-xl mx-auto flex justify-center items-center">
-      <div className="flex justify-center items-center flex-col">
-        <h1 className="font-bold text-heading text-4xl">
+    <div className='min-h-screen max-w-xl mx-auto flex justify-center items-center'>
+      <div className='flex justify-center items-center flex-col'>
+        <h1 className='font-bold text-heading text-4xl'>
           404 | Page Not Found
         </h1>
-        <Link
-          href={'/'}
-          className="text-heading hover:underline transition-all duration-300"
+        <button
+          onClick={() => router.back()}
+          className='text-heading hover:underline transition-all duration-300'
         >
-          Return to Home
-        </Link>
+          Return back
+        </button>
       </div>
     </div>
   )
