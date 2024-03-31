e2da30ca6c306d0a83e6b589905391701edc3d34
Gaks3 <dwi66116@gmail.com>
2024-03-31 15:21:25 +0700

    feat: user dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import HeaderList from '@/components/AnimeList/HeaderList'
 import { authUserSession } from '@/libs/auth'
 import Image from 'next/image'
+import Link from 'next/link'
 import { User } from 'react-feather'
 
 const Page = async () => {
@@ -9,16 +10,33 @@ const Page = async () => {
   return (
     <div>
       <HeaderList title="Dashboard" />
-      <div>
+      <div className="flex flex-col items-center gap-3">
         {user?.image ? (
           <Image
             src={user?.image}
             alt={user?.image ?? 'Profile Image'}
-            width={100}
-            height={100}
+            width={200}
+            height={200}
+            className="rounded-full"
           />
-        ) : null}
-        <p>{user?.name}</p>
+        ) : (
+          <User />
+        )}
+        <p className="text-heading font-semibold text-xl">{user?.name}</p>
+        <div className="flex flex-row gap-3">
+          <Link
+            href={'/dashboard/collection'}
+            className="bg-card2 px-2 py-2 rounded-md text-neutral-400 hover:bg-surface hover:text-white transition duration-200 shadow-lg"
+          >
+            Collection
+          </Link>
+          <Link
+            href={'/dashboard/comment'}
+            className="bg-card2 px-2 py-2 rounded-md text-neutral-400 hover:bg-surface hover:text-white transition duration-200 shadow-lg"
+          >
+            Comment
+          </Link>
+        </div>
       </div>
     </div>
   )
